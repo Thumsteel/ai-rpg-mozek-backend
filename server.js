75eea34167a0449d7eec344214bf6063a1534681
Tomáš Humlhanz <thumsteel@gmail.com>
2025-11-24 11:20:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post('/api/tah', async (req, res) => {
 
     try {
         const response = await ai.models.generateContent({
-            model: 'gemini-1.5-flash',
+            model: 'gemini-1.5-flash-001',
             contents: history,
             config: {
                 responseMimeType: "application/json",
